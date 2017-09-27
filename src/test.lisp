d3c88915274f572c72749e3f9f223924fa796992
i-makinori <maau3p@gmail.com>
2017-09-28 08:08:31 +0900

    [Add] test function which change piece-list pdata-string
    pdata is format of 2016's solver's piece-coordinate file

--- src/test.lisp
+++ src/test.lisp
@@ -1,4 +1,14 @@
 
+(in-package :procon)
 
+;;;; test utils ;;;;;;;;;;;;;;;
 
+(defun piece-list->2016-pdata (piece-list)
+  (let ((coord-seq-list (mapcar #'point-list->coord-sequence
+                                (mapcar #'(lambda (piece) (car-rest-assoc :points piece))
+                                        piece-list))))
+     (concat-string-list-list
+      (mapcar #'(lambda (list) 
+                  (format nil "~{~a ~}~%" list))
+       coord-seq-list))))
 
