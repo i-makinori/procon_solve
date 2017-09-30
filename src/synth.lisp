25363e4825389da7e4a93134371dd3e9b7514da3
i-makinori <maau3p@gmail.com>
2017-09-30 20:46:44 +0900

    [Add] simple-piece, function to transform simple piece

--- src/synth.lisp
+++ src/synth.lisp
@@ -3,31 +3,96 @@
 
 ;;;; synth-piece ;;;;;;;;;;;;;;;;
 
-;; Piece ::= {spots::[Spot], synth-from::Synth||Nil, synth-to::Synth||Nil}
-;; Synth ::= {Piece, direction::((+1||-1)::=::Bool), synth-from::Rotate-Ord}
-;; Spot ::= x::Int, y::Int
+;;;; struct 
 
-;; in hashtable, 
-;; element =:: table=::(Id::S), Piece
+(defstruct (easy-piece)
+  spots degrees is-frame)
 
-#|
-(defstruct (synth (:conc-name synth-))
-  piece direction synth-from)
+(defun easy-piece (spots degrees is-frame)
+  "structure make it more useful to synth piece.
+easy-piece :: [spot], [degree], is-frame
 
-(defun synth (piece direction synth-from-order-of-spot)
-  "make-synth"
-  (make-synth 
-   :piece piece
-   :direction direction
-   :synth-from synth-from-order-of-spot))
+which can intepret special synth"
+  (make-easy-piece :spots spots
+                   :degrees degrees
+                   :is-frame is-frame))
 
-(defstruct (piece (:conc-name piece-))
-  spots degrees synth-from synth-to)
+(defun easy-piece->piece (easy-piece)
+  (piece (easy-piece-spots easy-piece)
+         (easy-piece-degrees easy-piece)
+         (easy-piece-is-frame easy-piece)
+         nil nil))
+         
+(defun piece-from--easy-piece+synth+synth (easy-piece synth-from synth-to)
+  (piece (easy-piece-spots easy-piece)
+         (easy-piece-degrees easy-piece)
+         (easy-piece-is-frame easy-piece)
+         synth-from synth-to))
 
-(defun piece (spots degrees synth-from synth-to)
-  "make-piece"
-  (make-piece :spots spots
-              :degrees degrees
-              :synth-from synth-from
-              :synth-to synth-to))
-|#
+;;;; params 
+
+(defparameter plus +1)
+(defparameter minus -1)
+
+
+;;;; function 
+(defun synth->maybe-piece (synth-from synth-to)
+  (let* 
+      ((piece1 (synth-piece synth-from))
+       (piece1-spots (piece-spots piece1))
+       (piece1-degs (piece-degrees piece1))
+       
+       (piece2 (synth-piece synth-to))
+       (piece2-spots (piece-spots piece2))
+       (piece2-degs (piece-degrees piece2))
+       
+       (angle2 (vector-angle (spot->vec (rotate-nth (synth-synth-from synth-to)
+                                                    piece2-spots))
+       ))
+       
+       
+       )
+
+    angle2
+    #|
+    (piece
+     '() '() '()
+     synth1 synth2))
+    |#
+
+    ))
+
+(defun synth->easy-piece (synth)
+  "synth -> synth
+function make it more useful synth to synth piece"
+  (let* ((piece (synth-piece synth))
+         (direction (synth-direction synth))
+         (rotate-num (synth-synth-from synth))
+         ;;
+         (if-reverse (if (equal direction plus) #'id #'reverse))
+         (rotated-spots (rotate-list (piece-spots piece) rotate-num))
+         (rotated-degrees (rotate-list  (piece-degrees piece) rotate-num))
+         (origin-spot (nth (synth-synth-from synth) (piece-spots piece))))
+    (easy-piece 
+     (mapcar #'(lambda (spot)
+                 (spot (- (spot-x spot) (spot-x origin-spot))
+                       (- (spot-y spot) (spot-y origin-spot))))
+             (cons (car rotated-spots)
+                   (funcall if-reverse (cdr rotated-spots))))
+     (cons (car rotated-degrees)
+           (funcall if-reverse (cdr rotated-degrees)))
+     (piece-is-frame piece))))
+
+
+
+;;;; test
+
+;; ref : *test-piece-for-synth1* *test-piece-for-synth2*
+(defparameter *test-synth1* 
+  (synth *test-piece-for-synth1* plus 5))
+
+(defparameter *test-synth2*
+  (synth *test-piece-for-synth2* minus 4))
+
+(defparameter *test-synthed-piece*
+  (synth->maybe-piece *test-synth1* *test-synth2*))
