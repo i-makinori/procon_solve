8d5a11edd675690c48f9ebdddcb27816dd761b87
i-makinori <maau3p@gmail.com>
2024-02-10 10:42:33 +0900

    Update. SVG viewer use svg-pan-zoom library. also use cl-template for HTML template

--- src/render-html.lisp
+++ src/render-html.lisp
@@ -4,19 +4,6 @@
 ;;;; render results to HTML
 ;; HTML, SVG
 
-(defparameter *html-template* "
-<!DOCTYPE html>
-<html>
-  <head> </head>
-  <body> ~A ~A <hr /> </body>
-</html>")
-
-(defun html-text-by-template (body-content)
-  (format nil *html-template*
-          "<h1>solved-puzzle</h1>"
-          body-content
-          ))
-
 (defparameter *display-scale-multiply* 14)
 
 (defun vec3-list-into-svg-polygon (vec3-list)
@@ -32,7 +19,7 @@
     (format nil "<polygon points='~A' ~A />~%"
             vec3-text style)))
 
-(defun vec3-list-list-into-svg (piece-points-list)
+(defun vec3-list-list-into-svg (piece-points-list &key (id-string "shape_whole"))
   "piece vector list(piece) list into svg text"
   (let* ((polygon-list (mapcar #'(lambda (piece-points)
                                    (vec3-list-into-svg-polygon piece-points))
@@ -40,8 +27,8 @@
          (polygon-list-text
            (apply #'concatenate 'string polygon-list)))
     
-    (format nil "<svg width='400' height='400'>~%~A~%</svg>~%" polygon-list-text)))
-
+    (format nil "<svg id='~A' width='600' height='600'>~%~A~%</svg>~%"
+            id-string polygon-list-text)))
 
 
 (defun write-solven-puzzle-as-html (file-name puzzle)
@@ -49,15 +36,17 @@
   (let*
       ((pathname (merge-pathnames (format nil "test/results/~A" file-name) 
                                   *pathname-puzzle-1617-root*))
-       (svg-text (identity (vec3-list-list-into-svg (identity puzzle))))
-       (html-text (html-text-by-template svg-text)))
+       ;;(svg-text (identity (vec3-list-list-into-svg (identity puzzle))))
+       ;;(html-text (html-text-by-template svg-text)))
+       (html-text (template-text-of-solven-puzzle-html puzzle))
+       )
     (handler-case
         (progn (write-string-to-file pathname html-text)
                (format t "HTML file updated at : ~A ~%" pathname)
                pathname)
       (error (e) (print e) nil))))
 
-
+#|
 (defun write-point-list-list-as-html (file-name puzzle)
   "for piece list"
   (let*
@@ -70,7 +59,24 @@
                (format t "HTML file updated at : ~A ~%" pathname)
                pathname)
       (error (e) (print e) nil))))
+|#
 
+(defparameter *html-template-file* (merge-pathnames "src/viewer/htmlpage_template.html.clt"))
 
-
+(defun template-text-of-solven-puzzle-html (puzzle &key (template-file-path *html-template-file*))
+  "for solven puzzle"
+  (with-open-file (template-file template-file-path)
+    (let*
+        ((template (make-string (file-length template-file)))
+         (svg-alist (mapcar
+                     #'(lambda (puzzle id)
+                         `((:id . ,id)
+                           (:svg-text .
+                           ,(vec3-list-list-into-svg (identity puzzle) :id-string id))))
+                     ;; (list puzzle puzzle puzzle) (list "piyo" "fuga" "hoge") ;; listing example
+                     (list puzzle) (list "puzzle_solution")
+                     )))
+      (read-sequence template template-file)
+      (funcall (cl-template:compile-template template)
+               (list :svgs svg-alist)))))
 
