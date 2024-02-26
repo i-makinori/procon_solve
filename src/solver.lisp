3c84de5a5ff9ae191b85413ae1890192c570a4c1
i-makinori <maau3p@gmail.com>
2024-02-27 07:51:55 +0900

    Add. list of piece of synthesized piece

--- src/solver.lisp
+++ src/solver.lisp
@@ -1,15 +1,45 @@
 
 (in-package :puzzle-1617)
 
-;;;; 
+;;;; solver
 
 ;;; all synthesizeable patterns
+
 (defun all-synthesizeable-patterns-of-pieces-to-frame (frame piece-list)
   (flatten
    (remove nil 
            (mapcar #'synthesize-piece-to-frame-by-selection-piece-or-fail
                    (whole-set-of-point-and-edge-selections-pieces-to-frame
                     frame piece-list)))))
+
+
+;;;
+
+(defun list-of-piece-of-synthesized-piece (synthesized-piece)
+  (labels
+      ((aux (p_n)
+         (format t "==============~%~A~%" p_n)
+         (cond ((null p_n) nil)
+               (t
+                (append
+                 ;;(list (piece-id p_n))
+                 (list p_n)
+                 (if (piece-transform1 p_n)
+                     (aux (transform-piece (piece-transform1 p_n)))
+                     nil)
+                 (if (piece-transform2 p_n)
+                     (aux (transform-piece (piece-transform2 p_n)))
+                     nil))))))
+    (aux synthesized-piece)))
+
+
+(defun list-of-primary-piece-id-list-of-synthesized-piece (synthesized-piece)
+  )
+
+(defun sort-by-delta_points (synthesized-piece-list)
+  
+  )
+
 
 ;;;
 
