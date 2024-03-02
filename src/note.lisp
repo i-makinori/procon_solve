9c0be86af27861eef2cf6f0fc29c937f726959ab
i-makinori <maau3p@gmail.com>
2024-03-02 16:12:58 +0900

    Add. profiling procedures.

--- src/note.lisp
+++ src/note.lisp
@@ -19,6 +19,24 @@
 
 ;;;; profile
 
+(defmacro profile-form (&body forms)
+  (time
+   `(progn
+     ;; call profiler
+     (sb-sprof:start-profiling) 
+     
+     ;; actual calculation
+     
+     ;;(write-piece-list-as-html (remove-congruent-from-synthesized-piece-list search1))
+     ,@forms
+     
+     ;; stop and report
+     (sb-sprof:stop-profiling)    
+     (sb-sprof:report)
+     ;;
+     )) ;; and report at last  
+  )
+
 (defun profile-remove-congruents ()
   (let ((search1
           (sort-by-delta_points
@@ -38,6 +56,23 @@
        (sb-sprof:report)
        ;;
        )) ;; and report at last
+    nil
+    ))
+
+(defun profile-remove-congruents ()
+  (let ((search1
+          (sort-by-delta_points
+           (all-synthesizeable-patterns-of-pieces-to-frame
+            (car *example-problem-9*) (cdr *example-problem-9*)))))
+    (profile-form
+      (write-piece-list-as-html (remove-congruent-from-synthesized-piece-list search1))
+      )))
+
+
+(defun profile-easy-problem ()
+  (profile-form
+    (search-solution-from-prime-pieces
+     (cons (car *example-problem-9*) (cdr *example-problem-9*)))
     nil
     ))
 
