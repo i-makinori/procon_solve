7c5d50c8ea96351229ce4a0f6f5e9537d6671948
i-makinori <maau3p@gmail.com>
2024-03-04 13:26:50 +0900

    Clean. solver

--- src/solver.lisp
+++ src/solver.lisp
@@ -16,33 +16,41 @@
  (cons (car *example-problem-9*) (cdr *example-problem-9*)))
 |#
 
+(defun remove-piece-list-from-synthesized-piece-list
+    (state primary-piece-list synthesized-piece-list)
+  (remove-plus-piece-overs-frame-from-synthesized-piece-list
+   (remove-no-future-shaped-piece-from-synthesized-piece-list
+    (remove-congruent-from-synthesized-piece-list
+     (sort-by-delta_points
+      synthesized-piece-list))
+    primary-piece-list)
+   ;; todo. this is once old frame. frame of this step is may be better
+   (assocdr :frame state)))
+
 (defun search-solution-aux (stack-of-states primary-piece-list)
   (let* ((state (car stack-of-states))
          (stacking (cdr stack-of-states))
          ;;
          (piece-frame         (assocdr :frame state))
          (primary-piece-using (list-of-primary-piece-list-of-synthesized-piece piece-frame))
-         (primary-piece-rests (list-of-unused-primary-piece-list-of-synthesized-piece 
-                               piece-frame primary-piece-list))
+         ;;(primary-piece-rests (list-of-unused-primary-piece-list-of-synthesized-piece 
+         ;;piece-frame primary-piece-list))
          ;;
          )
     (format t "synth-list to: ~A, using-pieces [len]: ~A[~A]~%"
             (piece-id piece-frame)
             (mapcar #'piece-id primary-piece-using)
             (length primary-piece-using))
-    (let* ((patterns-of-step
-             (remove-plus-piece-overs-frame-from-synthesized-piece-list
-              (remove-no-future-shaped-piece-from-synthesized-piece-list
-               (remove-congruent-from-synthesized-piece-list
-                (sort-by-delta_points
-                 (all-synthesizeable-patterns-of-pieces-to-frame
-                  piece-frame primary-piece-rests)))
-               primary-piece-list)
-              ;; todo. this is once old frame. frame of this step is may be better
-              (assocdr :frame state)))
+    (let* (;; Synthesized Piece List
+           (spl-all-combinations
+             (all-synthesizeable-patterns-of-pieces-to-frame
+              (assocdr :frame state) primary-piece-list))
+           (spl-filtered ;; patterns-of-step
+             (remove-piece-list-from-synthesized-piece-list
+              state primary-piece-list spl-all-combinations))
            (states-of-step! (mapcar #'(lambda (next-frame)
                                         `((:frame . ,next-frame)))
-                                    patterns-of-step))
+                                    spl-filtered))
            (next-stack (sorted-states-by-evaluation-function
                         #'evaluation-value-by-delta-points
                         (append states-of-step! stacking)
