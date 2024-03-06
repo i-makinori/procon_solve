502c8b9863669a3c4fa43a3688f88011eaaeaf2b
i-makinori <maau3p@gmail.com>
2024-03-06 10:32:17 +0900

    Fix. beam search. into beam start at initial time (t1) state taken by initial state (fss_t1).

--- src/search-beam.lisp
+++ src/search-beam.lisp
@@ -26,17 +26,7 @@
 ;;; functions for Beam Search
 
 (defun beam-next (beam-previous next-stack-by-previous)
-  ;; (make-beam :index :depth :stack)
-  ;;
-  #|
-  (setf (beam-index beam-previous!)
-        (beam-index beam-previous!))
-  (setf (beam-depth beam-previous!)
-        (1+ (beam-depth beam-previous!)))
-  (setf (beam-stack beam-previous!)
-        (cdr (beam-stack beam-previous!)))
-  beam-previous!)
-|#
+  ;; proceed beam
   (make-beam :index (beam-index beam-previous)
              :depth (1+ (beam-depth beam-previous))
              :stack next-stack-by-previous))
@@ -57,23 +47,16 @@
 ))
 |#
 
-(defun select-head-n-of-stack-into-n-beams (n next-stack-by-previous beam-previous)
-  ;; todo: fix the selection of next state(stack_n) take from wide area of tree.
+(defun generate-initial-beams (n initial-state-stack)
   (mapcar
-   #'(lambda (n) n
-       (let ((next-index (if (> n 0)
-                             (progn (incf *beam-current-index*)
-                                    *beam-current-index*)
-                             (beam-index beam-previous))))
-         (make-beam :index next-index
-                    :depth (1+ (beam-depth beam-previous))
-                    ;; todo: incorrect restart node
-                    ;;:stack (nthcdr n next-stack-by-previous))))
-                    :stack (list (nth n next-stack-by-previous)))))
+   #'(lambda (n)
+       (incf *beam-current-index*)
+       (make-beam :index n
+                  :depth 0
+                  :stack (list (nth n initial-state-stack))))
    (remove-if #'(lambda (n) 
-                  (null (nthcdr n next-stack-by-previous))) ;; no future
+                  (null (nth n initial-state-stack)))
               (from-m-to-n-list 0 (- n 1)))))
-
 
 
 ;;; Beam Search
@@ -92,30 +75,25 @@
     (format t "d/dt: ~A~%" (fs-d/dt-evaluation-value state-of-this-step))
     (let* ((states-of-next-step (states-of-next-step-from-1-state state-of-this-step
                                                                   primary-piece-list))
-           (next-stack-of-this-step (next-state-stack states-of-next-step stacking-of-this-step))
+           (next-stack-by-this-step (next-state-stack states-of-next-step stacking-of-this-step))
+           (next-beam-of-this-step (beam-next beam-of-this-step next-stack-by-this-step))
            ;;
-           (next-queues-by-this-step
-             (select-head-n-of-stack-into-n-beams
-              (- *beam-width* (length beam-queue) -1) ;; -1 is this-step
-              next-stack-of-this-step
-              beam-of-this-step))
-           
-           (next-queue (append rest-queue next-queues-by-this-step)))
+           (next-queue (append rest-queue (list next-beam-of-this-step))))
       ;; format after once list
-      (format-search-status-after next-stack-of-this-step)
+      (format-search-status-after next-stack-by-this-step)
       (format t "beam {type, depth}: {~A, ~A}~%"
               (beam-index beam-of-this-step)
               (beam-depth beam-of-this-step))
       ;; HTML
       (write-piece-list-as-html 
-       (mapcar #'(lambda (state) (fs-frame-piece state)) next-stack-of-this-step))
+       (mapcar #'(lambda (state) (fs-frame-piece state)) next-stack-by-this-step))
       ;;(incf *n-search-iter*)
-      (cond ((null next-stack-of-this-step) ;; no methods in this beam's stack
+      (cond ((null next-stack-by-this-step) ;; no methods in this beam's stack
              (format t "=== the Beam_n is Cut By Dead Twig ===~%")
              (search-solution-aux-beam rest-queue primary-piece-list))
-            ((state-is-solution-p (car next-stack-of-this-step))
+            ((state-is-solution-p (car next-stack-by-this-step))
              ;; car is solution, however return all
-             next-stack-of-this-step
+             next-stack-by-this-step
              )
             ;; todo: cut by n-count and restart again
             ;;((> *n-search-iter* *n-search-iter-max*)
@@ -124,28 +102,37 @@
             (t ;; search-next
              (search-solution-aux-beam next-queue primary-piece-list))))))
 
+
 (defun search-solution-from-prime-pieces-beam (whole-primary-piece-list)
   (let* ((primary-pieces (remove-if-not #'primary-piece-p whole-primary-piece-list))
          (frame-pieces   (remove-if-not #'(lambda (p) (shape-minus-p (piece-pm-sign p)))
                                         primary-pieces)))
     (setf *n-search-iter* 0)
     (setf *beam-current-index* 0)
+    ;;    (setf *beam-width* 6)
+
+    ;;
     (cond
       ((not (= 1 (length frame-pieces)))
        (warn (format nil "whole-piece-list has multiple frames. IDs: ~A~%"
                      (mapcar #'piece-id frame-pieces)))
        nil)
       (t 
-       (let* ((frame-piece    (car frame-pieces))
-              ;;
+       (let* (;; frame and primes
+              (frame-piece    (car frame-pieces))
               (none-frame-pieces
                 (list-of-unused-primary-piece-list-of-synthesized-piece frame-piece primary-pieces))
-              (stack0 (list (make-fs-from-piece frame-piece none-frame-pieces)))
-              ;;
-              (stack-of-states_t0
-                (list (make-beam :index 0
-                                 :stack stack0)))
-              (solution-and-paths (search-solution-aux-beam
-                                   stack-of-states_t0 none-frame-pieces)))
+              ;; t0
+              (state_t0 (make-fs-from-piece frame-piece none-frame-pieces))
+              ;; t1
+              (stack_t1 (next-state-stack 
+                         (states-of-next-step-from-1-state state_t0 none-frame-pieces)
+                         '()))
+              ;; beams by t1
+              (beam-queue_t1 ;; first beam queue 
+                (generate-initial-beams *beam-width* stack_t1))
+              ;; search solution in tree by beam
+              (solution-and-paths
+                (search-solution-aux-beam beam-queue_t1 none-frame-pieces)))
          (mapcar #'(lambda (s) (fs-frame-piece s)) solution-and-paths)
          )))))
